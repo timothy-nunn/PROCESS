eac5bfdd0154f47a8826f851ae51b5c46f41f240
Clair Mould <86794332+clmould@users.noreply.github.com>
2025-04-23 11:40:16 +0100

    parse the example file tree in one for loop instead of two, use getattr to get the initial values of the variables

--- dicts_test/new_python_dicts.py
+++ dicts_test/new_python_dicts.py
@@ -24,39 +24,50 @@ def data_structure_dicts():
     var_names_and_descriptions = {}
     var_names_and_descriptions = {}
 
+    # look through the nodes in the tree in a pairwise manner
+    # want to do this to find things of the form: variable initialised \n variable docstring
+
     # if node is ast.AnnAssign, this is the vars being initialised so need to add to list of var names
-    # if node is ast.AnnAssign then add its initial value to the list (= None) (will update later on - see below)
-    # if ast.Assign then this is assigning the initial values in the init_example_variables fn, so update
-    # the initial values dict with these (will overwrite the None in the dict)
+    # if node is ast.AnnAssign then add its initial value to the list (= None) (will update later on using getattr)
+
     # need to check for pairs of ast.AnnAssign followed by an ast.Expr - this is the form of
     # a variable being declared followed by a docstring expression. can get these var descriptions
     # from here, and if there is no ast.Expr immediately after an ast.AnnAssign then this var does not
     # have a docstring and so set the description to be ""
-    for node in ast.walk(example_module_tree):
-        if isinstance(node, ast.AnnAssign):
-            initial_values_dict[node.target.id] = node.value.value
-            var_name = node.target.id
+
+    # use the variable name list and getattr to get the initial values of the variables from the example_module
+
+    for a, b in pairwise(example_module_tree.body):
+        if isinstance(a, ast.AnnAssign):
+            # get the variable names
+            initial_values_dict[a.target.id] = a.value.value
+            var_name = a.target.id
+            # add variable name to the list if not already there
             if var_name not in variable_names:
                 variable_names.append(var_name)
-        if isinstance(node, ast.Assign):
-            initial_values_dict[node.targets[0].id] = node.value.value
-    # doing as 2 separate for loops for now - example_tree.body only has AnnAssign, Expr and FunctionDef
-    # components, and no Assign components (unless you look inside the FunctionDef, will investigate combining
-    # into one for loop)
-    for a, b in pairwise(example_module_tree.body):
-        if isinstance(a, ast.AnnAssign) and isinstance(b, ast.Expr):
-            var_names_and_descriptions[a.target.id] = b.value.value
-        if isinstance(a, ast.AnnAssign) and not isinstance(b, ast.Expr):
-            var_names_and_descriptions[a.target.id] = ""
-    # create each of the 3 components seen in what_you_want.json
+            # if docstring immediately follows the variable declaration, add docstring to descriptions dict
+            if isinstance(b, ast.Expr):
+                var_names_and_descriptions[a.target.id] = b.value.value
+            # if no docstring for variable, have a blank description
+            if not isinstance(b, ast.Expr):
+                var_names_and_descriptions[a.target.id] = ""
+
+    # for each variable in the file, get the initial value (either is None, or value initialised in init_example_variables fn)
+    for var in variable_names:
+        initial_values_dict[var] = getattr(example_module, var)
+
     dict_module_entry["example_module"] = variable_names
+
+    # create each of the 3 dict components seen in what_you_want.json
     module_dict["DICT_MODULE"] = dict_module_entry
     default_dict["DICT_DEFAULT"] = initial_values_dict
     descriptions_dict["DICT_DESCRIPTIONS"] = var_names_and_descriptions
+
     # create combined dict to go in json file
     new_dict = {**default_dict, **descriptions_dict, **module_dict}
+
     # create json file
-    with open(Path("./dicts_test/data/example_dicts_recreated.json"), "w") as f:
+    with open(Path("./dicts_test/data/example_dicts_recreated3.json"), "w") as f:
         json.dump(new_dict, f, indent=4)
 
 
