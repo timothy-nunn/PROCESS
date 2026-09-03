2f5d2a9af71c9b1a9937044e7c3744bb619229a1
Timothy Nunn <timothy.nunn@ukaea.uk>
2026-09-03 10:59:42 +0100

    Parameterify Blanket and Build

--- process/data_structure/blanket_variables.py
+++ process/data_structure/blanket_variables.py
@@ -19,6 +19,8 @@ Acronyms for this module:
 from dataclasses import dataclass
 from enum import IntEnum, unique
 
+from process.core.data_structure.parameter import Parameter, PROCESSModelData
+
 
 @unique
 class BlktModelTypes(IntEnum):
@@ -29,175 +31,175 @@ class BlktModelTypes(IntEnum):
 
 
 @dataclass(slots=True)
-class BlanketData:
+class BlanketData(PROCESSModelData):
     """Dataclass holding blanket variables"""
 
-    vol_shld_inboard: float = 0.0
+    vol_shld_inboard: Parameter[float] = 0.0
     """Volume of inboard shield (m3)"""
 
-    vol_shld_outboard: float = 0.0
+    vol_shld_outboard: Parameter[float] = 0.0
     """Volume of outboard shield (m3)"""
 
-    vol_vv_inboard: float = 0.0
+    vol_vv_inboard: Parameter[float] = 0.0
     """Volume of inboard Vacuum Vessel (m3)"""
 
-    vol_vv_outboard: float = 0.0
+    vol_vv_outboard: Parameter[float] = 0.0
     """Volume of outboard Vacuum Vessel (m3)"""
 
-    dz_pf_cryostat: float = 0.0
+    dz_pf_cryostat: Parameter[float] = 0.0
     """Clearance between uppermost PF coil and cryostat lid (m)"""
 
-    vfblkti: float = 0.0
+    vfblkti: Parameter[float] = 0.0
     """Inboard void fraction of blanket"""
 
-    vfblkto: float = 0.0
+    vfblkto: Parameter[float] = 0.0
     """Outboard void fraction of blanket"""
 
-    len_blkt_inboard_coolant_channel_radial: float = 0.0
+    len_blkt_inboard_coolant_channel_radial: Parameter[float] = 0.0
     """Inboard blanket coolant channel length (radial direction) (m)"""
 
-    len_blkt_outboard_coolant_channel_radial: float = 0.0
+    len_blkt_outboard_coolant_channel_radial: Parameter[float] = 0.0
     """Outboard blanket coolant channel length (radial direction) (m)"""
 
-    len_blkt_inboard_segment_toroidal: float = 0.0
+    len_blkt_inboard_segment_toroidal: Parameter[float] = 0.0
     """Inboard blanket mid-plane toroidal circumference for segment (m)"""
 
-    len_blkt_outboard_segment_toroidal: float = 0.0
+    len_blkt_outboard_segment_toroidal: Parameter[float] = 0.0
     """Outboard blanket mid-plane toroidal circumference for segment (m)"""
 
-    len_blkt_inboard_segment_poloidal: float = 0.0
+    len_blkt_inboard_segment_poloidal: Parameter[float] = 0.0
     """Inboard blanket segment poloidal length (m)"""
 
-    len_blkt_outboard_segment_poloidal: float = 0.0
+    len_blkt_outboard_segment_poloidal: Parameter[float] = 0.0
     """Outboard blanket segment poloidal length (m)"""
 
-    len_blkt_inboard_channel_total: float = 0.0
+    len_blkt_inboard_channel_total: Parameter[float] = 0.0
     """Inboard primary blanket flow lengths (m)"""
 
-    len_blkt_outboard_channel_total: float = 0.0
+    len_blkt_outboard_channel_total: Parameter[float] = 0.0
     """Outboard primary blanket flow lengths (m)"""
 
-    bzfllengi_liq: float = 0.0
+    bzfllengi_liq: Parameter[float] = 0.0
     """Inboard secondary blanket flow lengths (m)"""
 
-    bzfllengo_liq: float = 0.0
+    bzfllengo_liq: Parameter[float] = 0.0
     """Outboard secondary blanket flow lengths (m)"""
 
-    p_fw_inboard_nuclear_heat_mw: float = 0.0
+    p_fw_inboard_nuclear_heat_mw: Parameter[float] = 0.0
     """Inboard first wall nuclear heating (MW)"""
 
-    p_fw_outboard_nuclear_heat_mw: float = 0.0
+    p_fw_outboard_nuclear_heat_mw: Parameter[float] = 0.0
     """Outboard first wall nuclear heating (MW)"""
 
-    temp_fw_inboard_peak: float = 0.0
+    temp_fw_inboard_peak: Parameter[float] = 0.0
     """Inboard first wall peak temperature (K)"""
 
-    temp_fw_outboard_peak: float = 0.0
+    temp_fw_outboard_peak: Parameter[float] = 0.0
     """Outboard first wall peak temperature (K)"""
 
-    mflow_fw_inboard_coolant_total: float = 0.0
+    mflow_fw_inboard_coolant_total: Parameter[float] = 0.0
     """Inboard mass flow rate to remove inboard FW power (kg/s)"""
 
-    mflow_fw_outboard_coolant_total: float = 0.0
+    mflow_fw_outboard_coolant_total: Parameter[float] = 0.0
     """Outboard mass flow rate to remove inboard FW power (kg/s)"""
 
-    mflow_fw_coolant_total: float = 0.0
+    mflow_fw_coolant_total: Parameter[float] = 0.0
     """Total mass flow rate to remove inboard FW power (kg/s)"""
 
-    mflow_fw_inboard_coolant_channel: float = 0.0
+    mflow_fw_inboard_coolant_channel: Parameter[float] = 0.0
     """Inboard mass flow rate per coolant pipe (kg/s)"""
 
-    mflow_fw_outboard_coolant_channel: float = 0.0
+    mflow_fw_outboard_coolant_channel: Parameter[float] = 0.0
     """Outboard mass flow rate per coolant pipe (kg/s)"""
 
-    n_fw_inboard_channels: float = 0.0
+    n_fw_inboard_channels: Parameter[float] = 0.0
     """Inboard total number of first wall coolant channels"""
 
-    n_fw_outboard_channels: float = 0.0
+    n_fw_outboard_channels: Parameter[float] = 0.0
     """Outboard total number of first wall coolant channels"""
 
-    p_blkt_nuclear_heat_inboard_mw: float = 0.0
+    p_blkt_nuclear_heat_inboard_mw: Parameter[float] = 0.0
     """Neutron power deposited inboard blanket blanket (MW)"""
 
-    p_blkt_nuclear_heat_outboard_mw: float = 0.0
+    p_blkt_nuclear_heat_outboard_mw: Parameter[float] = 0.0
     """Neutron power deposited outboard blanket blanket (MW)"""
 
-    mflow_blkt_inboard_coolant: float = 0.0
+    mflow_blkt_inboard_coolant: Parameter[float] = 0.0
     """Inboard blanket mass flow rate for coolant (kg/s)"""
 
-    mflow_blkt_outboard_coolant: float = 0.0
+    mflow_blkt_outboard_coolant: Parameter[float] = 0.0
     """Outboard blanket mass flow rate for coolant (kg/s)"""
 
-    mflow_blkt_coolant_total: float = 0.0
+    mflow_blkt_coolant_total: Parameter[float] = 0.0
     """Total blanket mass flow rate for coolant (kg/s)"""
 
-    mfblkti_liq: float = 0.0
+    mfblkti_liq: Parameter[float] = 0.0
     """Inboard blanket mass flow rate for liquid breeder (kg/s)"""
 
-    mfblkto_liq: float = 0.0
+    mfblkto_liq: Parameter[float] = 0.0
     """Outboard blanket mass flow rate for liquid breeder (kg/s)"""
 
-    mfblkt_liq: float = 0.0
+    mfblkt_liq: Parameter[float] = 0.0
     """Blanket mass flow rate for liquid breeder (kg/s)"""
 
-    mftotal: float = 0.0
+    mftotal: Parameter[float] = 0.0
     """Total mass flow rate for coolant (kg/s)"""
 
-    n_blkt_inboard_channels: float = 0.0
+    n_blkt_inboard_channels: Parameter[float] = 0.0
     """Inboard total number of blanket coolant pipes"""
 
-    n_blkt_outboard_channels: float = 0.0
+    n_blkt_outboard_channels: Parameter[float] = 0.0
     """Outboard total number of blanket coolant pipes"""
 
-    mfblktpi: float = 0.0
+    mfblktpi: Parameter[float] = 0.0
     """Inboard mass flow rate per coolant pipe (kg/s)"""
 
-    mfblktpo: float = 0.0
+    mfblktpo: Parameter[float] = 0.0
     """Outboard mass flow rate per coolant pipe (kg/s)"""
 
-    vel_blkt_inboard_coolant: float = 0.0
+    vel_blkt_inboard_coolant: Parameter[float] = 0.0
     """Inboard coolant velocity in blanket (m/s)"""
 
-    vel_blkt_outboard_coolant: float = 0.0
+    vel_blkt_outboard_coolant: Parameter[float] = 0.0
     """Outboard coolant velocity in blanket (m/s)"""
 
-    htpmw_fwi: float = 0.0
+    htpmw_fwi: Parameter[float] = 0.0
     """Inboard first wall pumping power (MW)"""
 
-    htpmw_fwo: float = 0.0
+    htpmw_fwo: Parameter[float] = 0.0
     """Outboard first wall pumping power (MW)"""
 
-    htpmw_blkti: float = 0.0
+    htpmw_blkti: Parameter[float] = 0.0
     """Inboard blanket pumping power (MW)"""
 
-    htpmw_blkto: float = 0.0
+    htpmw_blkto: Parameter[float] = 0.0
     """Outboard blanket pumping power (MW)"""
 
-    htpmw_fw_blkti: float = None
+    htpmw_fw_blkti: Parameter[float] = None
     """Inboard fw and blanket pumping power (MW)"""
 
-    htpmw_fw_blkto: float = None
+    htpmw_fw_blkto: Parameter[float] = None
     """Outboard fw and blanket pumping power (MW)"""
 
-    dz_blkt_half: float = 0.0
+    dz_blkt_half: Parameter[float] = 0.0
     """Blanket internal half-height (m)"""
 
-    dz_shld_half: float = 0.0
+    dz_shld_half: Parameter[float] = 0.0
     """Shield internal half-height (m)"""
 
-    dz_vv_half: float = 0.0
+    dz_vv_half: Parameter[float] = 0.0
     """Vacuum vessel internal half-height (m)"""
 
-    deg_blkt_outboard_poloidal_plasma: float = 0.0
+    deg_blkt_outboard_poloidal_plasma: Parameter[float] = 0.0
     """Outboard blanket poloidal angle subtended by plasma (degrees)"""
 
-    f_deg_blkt_outboard_poloidal_plasma: float = 0.0
+    f_deg_blkt_outboard_poloidal_plasma: Parameter[float] = 0.0
     """Fraction of outboard blanket poloidal angle subtended by plasma (degrees)"""
 
-    deg_blkt_inboard_poloidal_plasma: float = 0.0
+    deg_blkt_inboard_poloidal_plasma: Parameter[float] = 0.0
     """Inboard blanket poloidal angle subtended by plasma (degrees)"""
 
-    f_deg_blkt_inboard_poloidal_plasma: float = 0.0
+    f_deg_blkt_inboard_poloidal_plasma: Parameter[float] = 0.0
     """Fraction of inboard blanket poloidal angle subtended by plasma (degrees)"""
 
 
